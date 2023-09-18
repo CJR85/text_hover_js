c903f61c268a1430e1c4f055bdd964961ebf69d0
Calvin Roberts <roberts.calvin85@gmail.com>
2023-09-18 23:04:59 +0100

    Add transition-delay and hue-rotate to span

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 let text = document.querySelector('.text');
 text.innerHTML = text.innerText
   .split('')
-  .map((letters, i) => `<span>${letters}</span>`)
+  .map(
+    (letters, i) =>
+      `<span style="transition-delay:${i * 40}ms; filter:hue-rotate(${
+        i * 30
+      }deg)">${letters}</span>`
+  )
   .join('');
